e85d5e145f31519cdf599a0d8d600cf1603587cc
Nadia202 <gracheva.nadia.01@mail.ru>
2021-10-02 09:17:45 +0700

    Добавьте файлы проекта.

---
+++ RegistrationCourse_Gracheva/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegistrationCourse_Gracheva
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Entities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new Entities();
+            ShowTable();
+        }
+
+        private void ShowTable()
+        {
+            DataGridRegistrations.ItemsSource = context.CourseRegistration.ToList();
+        }
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var row = DataGridRegistrations.SelectedItem as CourseRegistration;
+            if (row == null)
+            {
+                MessageBox.Show("Select row for deleting at first");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы уверены что хотите удалить эту строку данных?", "Подтверждение удаления",
+                MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                context.CourseRegistration.Remove(row);
+                context.SaveChanges();
+                ShowTable();
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var newRegistration = new CourseRegistration();
+            context.CourseRegistration.Add(newRegistration);
+            var win = new Window1(context, newRegistration);
+            win.ShowDialog();
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnEdit = sender as Button;
+            var currentRegistration = btnEdit.DataContext as CourseRegistration;
+            var win = new Window1(context, currentRegistration);
+            win.ShowDialog();
+        }
+    }
+}
---
+++ RegistrationCourse_Gracheva/Window1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace RegistrationCourse_Gracheva
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Entities context;
+        public Window1(Entities context, CourseRegistration currentRegistration)
+        {
+            InitializeComponent();
+            this.context = context;
+            CmbCourse.ItemsSource = context.Course.ToList();
+            CmbTrainer.ItemsSource = context.Trainer.ToList();
+            this.DataContext = currentRegistration;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveImage();
+            context.SaveChanges();
+            this.Close();
+        }
+        private void SaveImage()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image files: *.jpg, *.png|*.jpg, *.png";
+            openFile.ShowDialog();
+            if (openFile.FileName.Length != 0)
+            {
+                string nameFile = openFile.FileName;
+                byte[] image = File.ReadAllBytes(nameFile);
+                var reg = (CourseRegistration)this.DataContext;
+                reg.CertificateImage = image;
+                certificate.Source = new BitmapImage(new Uri(nameFile));
+            }
+        }
+    }
+}
